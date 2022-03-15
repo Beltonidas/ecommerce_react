58d1a185d16436ff617ae8369d747536378bc916
Tomas E. Schuenemann <70041852+Beltonidas@users.noreply.github.com>
2022-03-15 20:46:17 -0300

    a ver

--- src/Components/ItemListContainer/Item/Item.jsx
+++ src/Components/ItemListContainer/Item/Item.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-function Item({productoSimple}) {
+function Item(productoSimple) {
 
+  console.log(productoSimple)
 
   return (
     <div>
         <div className="card width: 18rem;">
             <img src="..." className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title" key={productoSimple.id}>Articulo: productoSimple.name</h5>
+                <h5 className="card-title" key={productoSimple.id}>Articulo: {productoSimple.name}</h5>
                 <p className="card-text">texto descriptivo.</p>
                 <a href="#hola" className="btn btn-primary">Go somewhere</a>
             </div>

d1b81fb03b394fcce3520312200b8d0082054aca
Tomas E. Schuenemann <70041852+Beltonidas@users.noreply.github.com>
2022-03-23 19:49:04 -0300

    Pre entrega 8

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import Nabvar from './Components/NabVar/Nabvar';
 import ItemsLIstContainer from './Components/ItemListContainer/ItemsLIstContainer';
 import ItemContador from './Components/ItemListContainer/ItemContador'
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
 
 function App() {
 
   return (
-    <div className="App">
-      <Nabvar/>
-      <h2>Flowers and Herbs</h2>
-      {/*<ItemsLIstContainer saludo= "Contenedor de imagenes"/>*/}
-      <ItemDetailContainer identificacion={1}/>
+    <Router>
+      <div className="App">
+        <Nabvar/>
+        <h2>Flowers and Herbs</h2>
+        <ItemsLIstContainer saludo= "Contenedor de imagenes"/>
+        {/*<ItemDetailContainer identificacion={1}/>*/}
+      </div>
 
-    </div>
+    </Router>
   );
 }
 

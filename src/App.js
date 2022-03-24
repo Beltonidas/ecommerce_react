9b7d5306f4845a34ecb65b548a2545e21d0340ac
Tomas E. Schuenemann <70041852+Beltonidas@users.noreply.github.com>
2022-03-24 16:31:40 -0300

    rutas del navbar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import ItemsLIstContainer from "./Components/ItemListContainer/ItemsLIstContaine
 import ItemContador from "./Components/ItemListContainer/ItemContador";
 import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  NavLink,
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    NavLink,
 } from "react-router-dom";
 import Home from "./Components/Home";
 import Contact from "./Components/Contact";
@@ -21,28 +21,46 @@ import OurFlowers from "./Components/OurFlowers";
 import Footer from "./Components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Nabvar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route
-            path="/category/:id"
-            exact
-            element={<ItemsLIstContainer saludo="hola" />}
-          />
-          <Route path="/item/:id" element={<ItemDetailContainer />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/Blog" element={<Blog />} />
-          <Route path="/Experience" element={<Experience />} />
-          <Route path="/OurFlowers" element={<OurFlowers />} />
-          <Route path="/Footer" element={<Footer />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Nabvar / >
+        <
+        Router >
+        <
+        Routes >
+        <
+        Route path = "/home"
+        exact element = { < Home / > }
+        /> <
+        Route path = "/category/:id"
+        exact element = { < ItemsLIstContainer saludo = "hola" / > }
+        /> <
+        Route path = "/item/:id"
+        element = { < ItemDetailContainer / > }
+        /> <
+        Route path = "/Contact"
+        element = { < Contact / > }
+        /> <
+        Route path = "/AboutUs"
+        element = { < AboutUs / > }
+        /> <
+        Route path = "/Blog"
+        element = { < Blog / > }
+        /> <
+        Route path = "/Experience"
+        element = { < Experience / > }
+        /> <
+        Route path = "/OurFlowers"
+        element = { < OurFlowers / > }
+        /> <
+        Route path = "/Footer"
+        element = { < Footer / > }
+        /> <
+        /Routes> <
+        /Router> <
+        /div>
+    );
 }
 
 export default App;
--- src/Components/NabVar/Nabvar.jsx
+++ src/Components/NabVar/Nabvar.jsx
@@ -1,38 +1,44 @@
 import React from 'react'
 import CardWidget from '../CardWidget'
 //import ShoppingCartIcon from '../resources/icons/ShoppingCartIcon';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function Nabvar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
   <div className="container-fluid mt-0 pt">
-    <a className="navbar-brand fs-3 text-light" href="#haydenRunFarm"> Hayden Run Farm </a>
+    <a className="navbar-brand fs-3 text-light" href="home"> Hayden Run Farm </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse mt-2" id="navbarText">
       <ul className="navbar-nav me-auto">
         <li className="nav-item">
-            <a className="nav-link active text-light" aria-current="page" href="#home"> Home </a>
+            <a className="nav-link active text-light" aria-current="page" href="home"> Home </a>
         </li>
         <li className="nav-item">
-              <a className="nav-link text-light" href="#OurFlowers"> Our Flowers </a>
+              <a className="nav-link text-light" href="OurFlowers"> Our Flowers </a>
         </li>
         <li className="nav-item">
-              <a className="nav-link text-light" href="#Experience"> Experience </a>
+              <a className="nav-link text-light" href="Experience"> Experience </a>
         </li>
         <li className="nav-item">
-              <a className="nav-link text-light" href="#About Our Farm"> About Our Farm </a>
+              <a className="nav-link text-light" href="AboutUs"> About Our Farm </a>
         </li>
         <li className="nav-item">
-              <a className="nav-link text-light" href="#Contact Us"> Contact Us </a>
+              <a className="nav-link text-light" href="Contact"> Contact Us </a>
         </li>
         <li className="nav-item">
-              <a className="nav-link text-light" href="#Blog">Blog </a>
+              <a className="nav-link text-light" href="Blog">Blog </a>
         </li>
       </ul>
-      <a className="nav-link text-light" href="#compras"> <CardWidget/> </a>
+      <a className="nav-link text-light" href="Cart"> <CardWidget/> </a>
     </div>
   </div>
 </nav>

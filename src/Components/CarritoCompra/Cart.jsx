d1f03992ac8c036a38e8b73185b0da4639e53665
Tomas E. Schuenemann <70041852+Beltonidas@users.noreply.github.com>
2022-03-28 23:51:30 -0300

    Lista de items

--- src/Components/CarritoCompra/Cart.jsx
+++ src/Components/CarritoCompra/Cart.jsx
@@ -3,10 +3,21 @@ import { useCartContex } from "./CartContex";
 
 function Cart() {
   const { cartList } = useCartContex();
-  console.log("mi cart tiene", cartList);
+  const listItemCartList = cartList.map((element) => (
+    <li key={element.id}>{element.id}</li>
+  ));
+  const sum = () => {
+    let sumTotal = 0;
+    cartList.forEach((element) => {
+      sumTotal = sumTotal + element.price;
+    });
+    console.log(sumTotal);
+  };
+
   return (
     <div>
       <h1>Carrito de compras</h1>
+      <ul>{listItemCartList}</ul>
     </div>
   );
 }
